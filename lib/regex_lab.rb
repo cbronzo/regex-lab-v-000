ab32c2b2ce505f9801169e4cfeeeb70d0f6d49e6
Catherine Bronzo <cbronzo@mac.com>
2018-06-10 04:31:26 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,10 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.match(//)
+  if text.match(//)
+    true
+else
+  false
 end
 
 def valid_phone_number?(phone)
